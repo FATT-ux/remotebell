e08348eca4e411f1927bdc6882f86c8794854281
Никита <fatt@MacBook-Air-Nikita.local>
2026-05-27 21:08:10 +0700

    Добавил http методы для удаления событий

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -5,25 +5,43 @@ import PopUp from "./PopUp";
 function Data ({day, dayIndex, dayRu, events}){
 
     const [state, setState] = useState(false);
+    const [currentEvents, setCurrentEvents] = useState(events)
+
     const popUp = () =>{
         setState(!state);
     }
     const close = () =>{
         setState(false);
     }
+
+    const deleteImg = (eventTime, index) =>{
+        fetch(`/api/schedules/days/events?day=${day}&time=${eventTime}`, {
+            method: 'DELETE'
+        });
+    
+        const newEvents = [];
+        for (let i = 0; i < currentEvents.length; i++){
+            if (i !== index){
+                newEvents.push(currentEvents[i]);
+            } 
+        }
+        setCurrentEvents(newEvents);
+    }
+    
+
     return(
         <div className="welcome-inner-list">
         <h2 className="dayRu">{dayRu}</h2>
         <ul>
-            {events.map((obj, index) => (
+            {currentEvents.map((obj, index) => (
                         <li className="welcome-items" key={index}>
-                            Время {obj.time}, {obj.filepath}, {obj.duration} сек. <img src={trashPng} alt="" className="close" />
+                            Время {obj.time}, {obj.filepath}, {obj.duration} сек. <img onClick={() => deleteImg(obj.time, index)} src={trashPng} alt="" className="close" />
                             <hr />
                         </li>
                     ))}
         </ul>
         <button className="btn btn-add" onClick={popUp}>Добавить</button>
-        {state == true ? <PopUp events={events} day={day} close={close} /> : null}
+        {state == true ? <PopUp events={currentEvents} day={day} close={close} /> : null}
         </div>
     )
 }
